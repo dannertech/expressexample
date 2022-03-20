c8d560b4f04233d104ab198b91156f2726027071
Diamonique Danner <danner@danner.tech>
2022-03-20 17:58:41 -0400

    added comments to better understand code

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
+//import express
 const express = require('express');
-
+//create an instance of express, development standards normally use app as the instance name
 const app = express();
-
+//use the get method to set what happens when we hit a certain route or url endpoint
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
